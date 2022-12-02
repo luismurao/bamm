36ea81d2881d8fc456e6faaff84b534c1a17b082
Luis Osorio <luismurao@gmail.com>
2022-12-02 13:03:43 -0600

    Update adj_mat.R

--- R/adj_mat.R
+++ R/adj_mat.R
@@ -1,16 +1,27 @@
 #' adj_mat: Function to compute the adjacency matrix of an area.
 #'
-#' @description Creates an adjacency matrix of an area of interest. This could be the accessible area (M) of a species or any geographic region of interest.
-#' @param modelsparse A setA object returned by the function \code{\link[bamm]{model2sparse}}.
+#' @description Creates an adjacency matrix of an area of interest.
+#' This could be the accessible area (M) of a species or any geographic
+#' region of interest.
+#' @param modelsparse A setA object returned by the function
+#' \code{\link[bamm]{model2sparse}}.
 #' @param ngbs Numeric. Number of neighbors (see details).
-#' @param eigen_sys Logical. If TRUE the eigen analsys of the adjancency matrix will be returned.
-#' @param which_eigs Numeric. Which eigen value and eigen vector will be returned.
-#' @return Returns an adjacency matrix of class sparseMatrix of n+2 x n columns (n number of the non-NA cells of grid_base) with the coordinates of the non-NA cells of grid_base.
+#' @param eigen_sys Logical. If TRUE the eigen analyses of the
+#' adjacency matrix will be returned.
+#' @param which_eigs Numeric. Which eigen value and eigen vector will
+#' be returned.
+#' @return Returns an adjacency matrix of class sparseMatrix of n+2 x n
+#' columns (n number of the non-NA cells of grid_base) with the coordinates
+#' of the non-NA cells of grid_base.
 #' @export
-#' @details The grid_base raster object is the area where the dispersal process will occur.
-#' The number of neighbors depends on the dispersal abilities of the species and the spatial resolution
-#' of the grid_base; for example, a species's with big dispersal abilities will move throughout more
-#' than 1 km^2 per day, so the idea is to give an approximate number of moving neighbors (pixels) per
+#' @details The grid_base raster object is the area where the dispersal
+#' process will occur.
+#' The number of neighbors depends on the dispersal abilities of the species
+#' and the spatial resolution
+#' of the grid_base; for example, a species's with big dispersal abilities
+#' will move throughout more
+#' than 1 km^2 per day, so the idea is to give an approximate number of moving
+#' neighbors (pixels) per
 #' unit of time.
 #'
 #' @examples
@@ -84,50 +95,4 @@ adj_mat <- function(modelsparse,ngbs=1,eigen_sys=FALSE,which_eigs=1){
 
   return(g_set0)
 }
-
-
-
-# Modifed: A slightly modification of the function
-# .adjacentUD (in raster package) from Robert Hijmans
-# Licence GPL v3
-
-
-
-.adjacentBAM <- function(x, cells, ngb) {
-
-  directions <- sum(ngb==1, na.rm=TRUE)
-
-  rs <- raster::res(x)
-  target <- cells
-  rn <- raster::raster(ngb)
-  center <- which(raster::values(rn)==0)
-  rc <- raster::rowFromCell(rn, center)
-  cc <- raster::colFromCell(rn, center)
-  xngb <- yngb <- ngb
-  xngb[] <- rep(1:ncol(ngb), each=nrow(ngb)) - cc
-  yngb[] <- rep(nrow(ngb):1, ncol(ngb)) - (nrow(ngb)-rc+1)
-  ngb[ngb != 1] <- NA
-  xngb <- stats::na.omit(as.vector( xngb * rs[1] * ngb))
-  yngb <- stats::na.omit(as.vector( yngb * rs[2] * ngb))
-
-  xy <- raster::xyFromCell(x, cells)
-  X <- unlist(lapply(as.vector(xy[,1,drop=FALSE]), function(z) z + xngb ))
-  Y <- unlist(lapply(as.vector(xy[,2,drop=FALSE]), function(z) z + yngb ))
-
-  d <- data.frame(X,Y)
-
-
-  cell <- rep(cells, each=directions)
-
-  d <- stats::na.omit(cbind(cell, raster::cellFromXY(x, d)))
-
-  attr(d, 'na.action') <- NULL
-  colnames(d) <- c('fromCell', 'toCell')
-  d <- d[d[,2] %in% target, ]
-
-  d <- d[order(d[,1], d[,2]),]
-
-  return(d)
-}
-
 
