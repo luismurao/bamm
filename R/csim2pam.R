ec06cc3542ea9f92cd81ca8efbf60941e1006bb3
Luis Osorio <luismurao@gmail.com>
2024-07-01 13:23:16 -0600

    Added coordinates to csim2pam

--- R/csim2pam.R
+++ R/csim2pam.R
@@ -48,8 +48,12 @@ csim2pam <- function(community_sim, which_steps){
 
   n_sps <- length(community_sim@community_sim)
   sps_names <- names(community_sim@community_sim)
+  xys <- raster::xyFromCell(community_sim@community_sim[[1]]@niche_model,
+                            community_sim@community_sim[[1]]@cellIDs)
+
   pamL <- lapply(which_steps, function(t_step){
     sim_t <- Matrix::t(community_sim@community_sim[[1]]@sdm_sim[[t_step]])
+    sim_t <- Matrix::cbind2(xys,sim_t)
     for (sps in 2:n_sps) {
       sim_tm <- community_sim@community_sim[[sps]]@sdm_sim[[t_step]]
       sim_t <- Matrix::cbind2(sim_t,Matrix::t(sim_tm))
